e3a2b8df53bd650ffe92c7ec396214f594847323
Simon Nyholm Sørensen <simonnyholm@htomail>
2022-06-09 12:52:40 +0200

    add app as primary route element

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route element={<App/>}>
+
+      </Route>
+    </Routes>
+  </BrowserRouter>
+   
+  
 );
 
