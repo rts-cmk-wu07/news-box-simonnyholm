5bdd8dc3a5f421d3bd4380419af258ec3dca6a5a
Simon Nyholm Sørensen <simonnyholm@htomail>
2022-06-09 12:52:05 +0200

    add color variables

---
+++ src/Color.js
@@ -0,0 +1,12 @@
+export const Color = {
+  sage: "#87bcbf",
+  rust: "#d97d54",
+  drab: "#324755",
+  danger: "#d95454",
+  onyx: "#1b1c20",
+  slate: "#6e8ca0",
+  snow: "#ffffff",
+  ice: "#f0f3f4",
+  fossil: "#c8d1d3",
+  sand: "#b9b0a2",
+};
