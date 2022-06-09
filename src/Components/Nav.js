cd876fd8d1395c0c0aa2e6bbcfcb9f67a0c76af9
Simon Nyholm Sørensen <simonnyholm@htomail>
2022-06-09 14:08:12 +0200

    Add nav

---
+++ src/Components/Nav.js
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+import IconLeftDiv from "./iconLeftDiv";
+
+const Nav = ({linkPath, linkIcon, appHeading}) => {
+    return ( <nav>
+        <Link path={linkPath}>
+            <IconLeftDiv/>
+            
+        </Link>
+        <h1>{appHeading}</h1>
+        <Link></Link>
+    </nav> );
+}
+ 
+export default Nav;
