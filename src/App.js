a2d69b8cd67e1c309c63ff83c5a83c54becd315f
Simon Nyholm Sørensen <simonnyholm@htomail>
2022-06-10 11:24:25 +0200

    add prettier

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Nav from "./components/Nav";
 import { Outlet } from "react-router-dom";
 
 function App() {
-  return <div className="App">
-    <Nav/>
-    <main>
-      <Outlet/>
-    </main>
-  </div>;
+  return (
+    <div className="App">
+      <Nav />
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
 }
 
 export default App;
