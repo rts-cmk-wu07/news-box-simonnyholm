da6f45e3e59d6a37c29ec1eeca358f9b3c323b2c
Simon Nyholm Sørensen <simonnyholm@htomail>
2022-06-10 11:28:00 +0200

    delete link tags from nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,7 @@
 import { Link } from "react-router-dom";
 
 const Nav = () => {
-  return (
-    <nav>
-      <Link></Link>
-      <h1>{}</h1>
-      <Link></Link>
-    </nav>
-  );
+  return <nav></nav>;
 };
 
 export default Nav;
