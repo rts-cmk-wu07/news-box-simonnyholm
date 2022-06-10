501539881c94b5b22437fecc0d94594a2ca7fe75
Simon Nyholm Sørensen <simonnyholm@htomail>
2022-06-10 12:26:26 +0200

    add sectionHead

--- src/components/CategorySectionArts.js
+++ src/components/CategorySectionArts.js
@@ -1,6 +1,15 @@
+import { FaNewspaper } from "react-icon/fa";
+
 const CategorySectionArts = () => {
   return (
     <section>
+      <div className="sectionHead">
+        <div className="sectionIcon">
+          <FaNewspaper />
+        </div>
+        <h2>Arts</h2>
+        <div className="unfoldIcon"></div>
+      </div>
       <CategoryListArts />
     </section>
   );
