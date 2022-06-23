35ea484e1f9f07e7c633796fef10c949d5b7523b
Simon Nyholm Sørensen <simonnyholm@htomail>
2022-06-23 15:40:29 +0200

    add test getItem

--- src/components/Archive.js
+++ src/components/Archive.js
@@ -1,7 +1,22 @@
+import { useEffect } from "react";
+
 const Archive = () => {
+
+    /*Brug nedenstående i hvert fetch*/
+  useEffect(() => {
+    const archItems = JSON.parse(localStorage.getItem("archive"));
+    console.log(archItems);
+
+    const findByCategory = archItems.filter((object) => {
+      return object.category === "Arts";
+    });
+
+    console.log("findByCategory", findByCategory);
+  }, []);
+
   return (
     <div>
-      <h2>Arkivsk</h2>
+      <h2>Archive</h2>
     </div>
   );
 };
