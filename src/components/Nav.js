023957a0992b1577b8bb5e86f05a84ca74a4e470
Carsten Lund <clu@rts.dk>
2022-06-24 15:40:21 +0200

    wrong updated nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,108 +1,16 @@
-/** @jsxImportSource @emotion/react */
-import { css } from "@emotion/react";
 import { Link } from "react-router-dom";
-import { BsInbox } from "react-icons/bs";
-import { IoSettings } from "react-icons/io5";
-import { Color } from "../Color";
-import { useState } from "react";
-import { IoIosArrowBack } from "react-icons/io";
+import IconLeftDiv from "./IconLeftDiv";
 
-
-
-const Nav = () => {
-const styles = {
-nav: css`
-display: flex;
-justify-content: space-between;
-border-bottom: 2px solid ${Color.sage};
-padding: 20px 0 20px 0;
-& div {
-margin: 4px 10px 0 10px;
-font-size: 25px;
-}
-& a {
-color: ${Color.onyx};
-}
-& h1 {
-font-size: 25px;
-margin: 0;
-}
-`,
+const Nav = ({ linkPath, linkIcon, appHeading }) => {
+  return (
+    <nav>
+      <Link path={linkPath}>
+        <IconLeftDiv />
+      </Link>
+      <h1>{appHeading}</h1>
+      <Link></Link>
+    </nav>
+  );
 };
-
-
-
-const [navHome, setNavHome] = useState(true);
-const [navSettings, setNavSettings] = useState(false);
-console.log("navHome", navHome);
-console.log("navSettings", navSettings);
-
-
-
-return (
-<>
-{navHome && (
-<nav css={styles.nav}>
-<div>
-<Link
-onClick={(event) => {
-setNavHome(false);
-setNavSettings(false);
-}}
-to="/archive"
->
-<BsInbox />
-</Link>
-</div>
-<h1>Newsbox</h1>
-<div>
-<Link
-to="/settings"
-onClick={(event) => {
-setNavHome(false);
-setNavSettings(true);
-}}
->
-<IoSettings />
-</Link>
-</div>
-</nav>
-)}
-{!navHome && (
-<nav css={styles.nav}>
-<div>
-<Link
-to="/"
-onClick={(event) => {
-setNavHome(true);
-setNavSettings(false);
-}}
->
-<IoIosArrowBack />
-</Link>
-</div>
-{navSettings && <h1>Settings</h1>}
-{!navSettings && <h1>Archive</h1>}
-<div>
-{!navSettings && (
-<Link
-to="/settings"
-onClick={(event) => {
-setNavHome(false);
-setNavSettings(true);
-}}
->
-<IoSettings />
-</Link>
-)}
-{navSettings && <div></div>}
-</div>
-</nav>
-)}
-</>
-);
-};
-
-
 
 export default Nav;
